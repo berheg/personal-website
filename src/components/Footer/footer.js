d38a06e2a38f860949eaaf5d28b38e9e3efb6cd0
Girmay <ink423103@yahoo.com>
2020-06-02 23:14:51 +0200

    Footer component added

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './footer.css';
+
+class Footer extends Component {
+  render() {
+    return(
+      <footer>
+        <section class="ft-legal">
+          <ul class="ft-legal-list">
+            <li><a href="#">Terms &amp; Conditions</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li>&copy; 2019 Copyright Nowrap Inc.</li>
+          </ul>
+        </section>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
